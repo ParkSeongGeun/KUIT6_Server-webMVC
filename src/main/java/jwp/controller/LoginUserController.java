063cebc4909a35db9579f1ff86f61a92a86c46cc
ParkSeongGeun <phd0801@naver.com>
2025-10-03 19:03:01 +0900

    [Feat] Assignment 1 - Requirement 1: Login

---
+++ src/main/java/jwp/controller/LoginUserController.java
@@ -0,0 +1,41 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * 사용자 로그인을 처리하는 컨트롤러
+ * - userId와 password를 받아서 사용자 인증을 수행하고,
+ * - 성공 시 세션에 사용자 정보를 저장한 후 메인 페이지로 리다이렉트
+ * Request Param: userId(String), password(String)
+ * Response : success -> ("/") redirect, fail -> ("/user/loginFailed.jsp")
+ */
+@WebServlet("/user/login")
+public class LoginUserController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+
+        User user = MemoryUserRepository.getInstance().findUserById(userId);
+
+        if (user != null && user.matchPassword(password)) {
+            // 로그인 성공 - 세션에 사용자 정보 저장
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("/");
+        } else {
+            // 로그인 실패
+            response.sendRedirect("/user/loginFailed.jsp");
+        }
+    }
+}
